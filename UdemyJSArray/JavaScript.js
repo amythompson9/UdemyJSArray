55ef9a9570a73da69a63c6b835fe988addb5dffd
Amy Thompson <amythompson9@gmail.com>
2018-06-22 18:45:10 -0400

    Array Method

--- UdemyJSArray/JavaScript.js
+++ UdemyJSArray/JavaScript.js
@@ -1,1 +1,13 @@
-﻿
+﻿var colors = ["red", "orange", "yellow"];
+
+//colors
+//returns ["red", "orange", "yellow"]
+
+//colors[1]
+//returns "orange"
+
+//add in green
+//colors[3] = "green"
+
+//change colors
+//colors[3] = "dark green";
