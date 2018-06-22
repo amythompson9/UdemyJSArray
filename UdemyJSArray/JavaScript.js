956dac62961e494902a41d16a02f55734f78fd10
Amy Thompson <amythompson9@gmail.com>
2018-06-22 18:54:33 -0400

    Array Test

--- UdemyJSArray/JavaScript.js
+++ UdemyJSArray/JavaScript.js
@@ -11,3 +11,21 @@
 
 //change colors
 //colors[3] = "dark green";
+
+var numbers = [22, 67, 33, 96, 88];
+//What does the following line print?
+console.log(numbers[numbers.length]);
+//undefined
+
+//nested or 2 dimensional array
+var friendGroups = [
+    ["Harry", "Ron", "Hermione"],
+    ["Malfoy", "Crabbe", "Goyle"],
+    ["Mooney", "Wormtail", "Prongs"],
+];
+
+//What is the result of this line?
+console.log(friendGroups[2][0]);
+//Mooney
+//console.log(friendGroups[2]); ["Mooney", "Wormtail", "Prongs"],
+//then the [0] index of that is Mooney
